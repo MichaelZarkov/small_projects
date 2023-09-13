6ac4b09dda30456664fd34b732271056f23d99bc
MichaelZarkov <61320281+MichaelZarkov@users.noreply.github.com>
2023-09-13 11:49:21 +0300

    Delete console_raster_image_editor directory

--- console_raster_image_editor/Source code/Image_Editor_Factory.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Image_Editor.h"
-
-class Image_Editor_Factory
-{
-public:
-	Image_Editor* createEditor(const char* fileName) const;
-};
--- console_raster_image_editor/Source code/main.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "Session_Manager.h"
-
-int main()
-{
-	Session_Manager obj;
-}
